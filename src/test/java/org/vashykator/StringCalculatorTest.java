2658a5d8c6879f96b883931f24f9fa63425b40c2
Timoty Granziero <timoty.granziero@arcavita.it>
2021-10-08 09:34:00 +0200

    new StringCalculatorFactory().create() in test

--- src/test/java/org/vashykator/StringCalculatorTest.java
+++ src/test/java/org/vashykator/StringCalculatorTest.java
@@ -7,11 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
     private final StringCalculator stringCalculator =
-            new StringCalculator(
-                    new CustomSeparatorSplitStrategyDecorator(new SplitByNewLineStrategyDecorator(new SplitByCommaStrategy())),
-                    new KeepNonEmptyFilterStrategy(),
-                    new SimpleStringToIntegerParseConverter(),
-                    new SimpleSumStrategy());
+            new StringCalculatorFactory().create();
 
     @Test
     @DisplayName("should return the sum of the two given integers")
